1630abe9efe3bf862f233e24f3d7a6a23331e1db
Joao Alfredo Gama Batista <joao.gama@savoirfairelinux.com>
2013-07-22 17:48:23 -0400

    New version based on the original

--- openerp_bootstrap/newmodule.py
+++ openerp_bootstrap/newmodule.py
@@ -1,3 +1,6 @@
+import datetime
+import os
+import shutil
 from paste.script import templates
 from paste.script.templates import var
 
@@ -19,8 +22,14 @@ class NewModule(templates.Template):
         var('author_email', 'Author email'),
         var('category', 'Category'),
         var('website', 'Website'),
-        var('depends', 'Dependencies [space-separated module names]',default=''),
-        var('is_web', 'Is web addon? [yes/no]', default='no'),    
+        var('license', 'License'),
+        var('depends', 'Dependencies [space-separated module names]',
+            default=''),
+        var('is_web', 'Is web addon? [yes/no]', default='no'),
+        var('has_demo', 'Has demo data? [yes/no]', default='no'),
+        var('has_report', 'Has reports? [yes/no]', default='yes'),
+        var('has_wizard', 'Has wizards? [yes/no]', default='yes'),
+        var('has_workflow', 'Has workflows? [yes/no]', default='no'),
     ]
 
     def pre(self, command, output_dir, vars):
@@ -30,6 +39,48 @@ class NewModule(templates.Template):
         # import pdb;pdb.set_trace()
         depends = vars['depends'].split(' ')
         vars['is_web'] = vars['is_web'] == 'yes' and True or False
+        vars['has_demo'] = vars['has_report'] == 'yes' and True or False
+        vars['has_report'] = vars['has_report'] == 'yes' and True or False
+        vars['has_wizard'] = vars['has_wizard'] == 'yes' and True or False
+        vars['has_workflow'] = vars['has_workflow'] == 'yes' and True or False
         if vars['is_web'] and not 'web' in depends:
             depends.append('web')
         vars['depends'] = [x for x in depends if x]
+
+        if vars['license'] in ('AGPL-3', 'AGPLv3', 'GPL-3', 'GPLv3'):
+            if vars['license'] in ('AGPL-3', 'AGPLv3'):
+                license = 'GNU Affero General Public License'
+            else:
+                license = 'GNU General Public License'
+
+            vars['copyright'] = """##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) {0} {1} (<{2}>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the {3} as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    {3} for more details.
+#
+#    You should have received a copy of the {3}
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################""".format(
+    datetime.date.today().year,
+    vars['author'],
+    vars['website'],
+    license)
+        else:
+            license = ''
+
+    def post(self, command, output_dir, vars):
+        for i in ('has_report', 'has_wizard', 'has_workflow'):
+            if not vars[i]:
+                shutil.rmtree(os.path.join(output_dir,
+                                           i.replace('has_', '')))
